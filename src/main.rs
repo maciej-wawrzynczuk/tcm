e58e25a73027709ef8994d3ce30ab61a04602253
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2026-09-06 20:58:12 +0200

    can get file content

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,12 @@
-use serde::Serialize;
-use tcm::runner::{CmdRunner, LocalCmdRunner, RunError};
-use thiserror::Error;
+use tcm::runner::LocalCmdRunner;
+use tcm::shfile::ShFile;
 
 #[tokio::main]
 async fn main() -> color_eyre::eyre::Result<()> {
     let l = LocalCmdRunner {};
     let f = ShFile::new(l).claim("/etc/hosts").await?;
-    let s = toml::to_string(&f)?;
-    println!("{s}");
+
+    println!("{}", f.get_content().await?);
 
     Ok(())
-}
-
-////////////////////////////// ShFile ///////////////////
-
-#[derive(Serialize)]
-struct ShFile<T: CmdRunner> {
-    #[serde(skip)]
-    r: T,
-    path: String,
-}
-
-impl<T: CmdRunner> ShFile<T> {
-    fn new(r: T) -> Self {
-        Self {
-            path: String::new(),
-            r,
-        }
-    }
-
-    async fn claim(mut self, filename: &str) -> Result<Self, ClaimError> {
-        let cmd = &["stat", "--format", "%F", filename];
-        let out = self.r.run(cmd).await?.trim().to_string();
-        if out.as_str() == "regular file" {
-            self.path = filename.to_string();
-            Ok(self)
-        } else {
-            Err(ClaimError::WrongFileType(out))
-        }
-    }
-}
-
-#[derive(Debug, Error)]
-enum ClaimError {
-    #[error("Unable to run command")]
-    RunError(#[from] RunError),
-    #[error("Wrong file type: {0}")]
-    WrongFileType(String),
 }
---
+++ src/shfile.rs
@@ -0,0 +1,50 @@
+use crate::runner::{CmdRunner, RunError};
+use serde::Serialize;
+use thiserror::Error;
+
+#[derive(Serialize)]
+pub struct ShFile<T: CmdRunner> {
+    #[serde(skip)]
+    r: T,
+    path: String,
+}
+
+impl<T: CmdRunner> ShFile<T> {
+    pub fn new(r: T) -> Self {
+        Self {
+            path: String::new(),
+            r,
+        }
+    }
+
+    pub async fn claim(mut self, filename: &str) -> Result<Self, ClaimError> {
+        let cmd = &["stat", "--format", "%F", filename];
+        let out = self.r.run(cmd).await?.trim().to_string();
+        if out.as_str() == "regular file" {
+            self.path = filename.to_string();
+            Ok(self)
+        } else {
+            Err(ClaimError::WrongFileType(out))
+        }
+    }
+
+    pub async fn get_content(&self) -> Result<String, ContentError> {
+        let cmd = &["cat", self.path.as_str()];
+        let out = self.r.run(cmd).await?.trim().to_string();
+        Ok(out)
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ClaimError {
+    #[error("Unable to run command")]
+    RunError(#[from] RunError),
+    #[error("Wrong file type: {0}")]
+    WrongFileType(String),
+}
+
+#[derive(Debug, Error)]
+pub enum ContentError {
+    #[error("Unable to run command")]
+    RunError(#[from] RunError),
+}
